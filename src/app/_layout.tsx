323ab98cce1012d0c03f7de0fbed039162ac9ff6
JAK-official <james.abou.khalil@gmail.com>
2026-06-22 15:41:25 +0300

    Added Db migrations and called them at app start

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,16 +5,17 @@ import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 import ListExample from '@/components/ui/list';
 import { ItemsProvider } from '@/context/itemsContext';
+import { migrate } from '@/database/migrations';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  migrate();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
       <ItemsProvider>
       <AppTabs />
       </ItemsProvider>
-      
     </ThemeProvider>
   );
 }
---
+++ src/database/migrations/001_create_tasks.ts
@@ -0,0 +1,30 @@
+import { db } from '../db';
+
+
+export function up() {
+
+  db.execSync(`
+    CREATE TABLE IF NOT EXISTS tasks (
+
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+      title TEXT NOT NULL,
+
+      completed INTEGER NOT NULL DEFAULT 0,
+
+      created_at TEXT NOT NULL
+
+    );
+  `);
+
+}
+
+
+
+export function down() {
+
+  db.execSync(`
+    DROP TABLE IF EXISTS tasks;
+  `);
+
+}
