2b095ddf660e420c7f89623836872f691f3c3951
JAK-official <james.abou.khalil@gmail.com>
2026-06-23 09:06:19 +0300

    Updated down() process to use latest version of code

--- src/database/migrations/002_add_position.ts
+++ src/database/migrations/002_add_position.ts
@@ -9,37 +9,7 @@ export function up() {
 
 export function down() {
   db.execSync(`
-    CREATE TABLE tasks_backup AS
-    SELECT
-      id,
-      title,
-      completed,
-      created_at
-    FROM tasks;
-
-
-    DROP TABLE tasks;
-
-
-    CREATE TABLE tasks (
-
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-      title TEXT NOT NULL,
-
-      completed INTEGER NOT NULL DEFAULT 0,
-
-      created_at TEXT NOT NULL
-
-    );
-
-
-    INSERT INTO tasks
-    SELECT *
-    FROM tasks_backup;
-
-
-    DROP TABLE tasks_backup;
-
+    ALTER TABLE tasks
+    DROP COLUMN position;
   `);
 }
